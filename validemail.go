4276d1ca6e25e92aa47efa549f6d8023a9a8f9a9
Andrii Stasiuk <as@ges.sh>
2019-05-30 15:24:49 +0200

    second version

--- validemail.go
+++ validemail.go
@@ -2,39 +2,25 @@ package validemail
 
 import "strings"
 
+// EMailChecker function checks if email is correct
 func EMailChecker(email string) bool {
 	s := strings.Split(email, "@")
 	if len(s) == 2 {
-		// println(s[0])
-		// println(s[1])
-		// println(string(s[1][len(s[1])-1]))
-
 		if !(byte(s[1][len(s[1])-1]) >= byte('A') && byte(s[1][len(s[1])-1]) <= byte('z')) {
-			println("false")
 			return false
 		}
-
 		for _, s0 := range s[0] {
 			if !(byte(s0) >= byte('A') && byte(s0) <= byte('z')) {
-				println("false")
 				return false
 			}
 		}
-
 		for _, s1 := range s[1] {
 			if !(byte(s1) >= byte('A') && byte(s1) <= byte('z') || (byte(s1) <= byte('.'))) {
-				println("false")
 				return false
 			}
 		}
 	} else {
-		println("false")
 		return false
 	}
-
 	return true
 }
-
-// func main() {
-// 	eMailChecker("as@ges.sh")
-// }
